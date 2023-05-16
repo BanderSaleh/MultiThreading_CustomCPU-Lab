db51619be37f513e68c4588f4cbdf181443e76da
Bander Saleh <bandersaleh93@gmail.com>
2023-05-16 13:54:53 -0700

    Initial Commit, WPF GUI Finalized Visually but Missing Code+Functionality

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace CustomCPU_Code
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnCallBack = new System.Windows.Forms.Button();
+            this.btnReturnValue = new System.Windows.Forms.Button();
+            this.btnStopWatch = new System.Windows.Forms.Button();
+            this.btnMultiThread = new System.Windows.Forms.Button();
+            this.btnAsync = new System.Windows.Forms.Button();
+            this.btnExample1 = new System.Windows.Forms.Button();
+            this.rtbDisplay = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(354, 352);
+            this.btnClear.Margin = new System.Windows.Forms.Padding(2);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(164, 25);
+            this.btnClear.TabIndex = 15;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            // 
+            // btnCallBack
+            // 
+            this.btnCallBack.Location = new System.Drawing.Point(656, 147);
+            this.btnCallBack.Margin = new System.Windows.Forms.Padding(2);
+            this.btnCallBack.Name = "btnCallBack";
+            this.btnCallBack.Size = new System.Drawing.Size(121, 33);
+            this.btnCallBack.TabIndex = 14;
+            this.btnCallBack.Text = "Callback";
+            this.btnCallBack.UseVisualStyleBackColor = true;
+            // 
+            // btnReturnValue
+            // 
+            this.btnReturnValue.Location = new System.Drawing.Point(656, 88);
+            this.btnReturnValue.Margin = new System.Windows.Forms.Padding(2);
+            this.btnReturnValue.Name = "btnReturnValue";
+            this.btnReturnValue.Size = new System.Drawing.Size(121, 33);
+            this.btnReturnValue.TabIndex = 13;
+            this.btnReturnValue.Text = "Return Value";
+            this.btnReturnValue.UseVisualStyleBackColor = true;
+            // 
+            // btnStopWatch
+            // 
+            this.btnStopWatch.Location = new System.Drawing.Point(24, 309);
+            this.btnStopWatch.Margin = new System.Windows.Forms.Padding(2);
+            this.btnStopWatch.Name = "btnStopWatch";
+            this.btnStopWatch.Size = new System.Drawing.Size(159, 31);
+            this.btnStopWatch.TabIndex = 12;
+            this.btnStopWatch.Text = "Stopwatch Example";
+            this.btnStopWatch.UseVisualStyleBackColor = true;
+            // 
+            // btnMultiThread
+            // 
+            this.btnMultiThread.Location = new System.Drawing.Point(24, 239);
+            this.btnMultiThread.Margin = new System.Windows.Forms.Padding(2);
+            this.btnMultiThread.Name = "btnMultiThread";
+            this.btnMultiThread.Size = new System.Drawing.Size(159, 31);
+            this.btnMultiThread.TabIndex = 11;
+            this.btnMultiThread.Text = "Run ( Multiple )";
+            this.btnMultiThread.UseVisualStyleBackColor = true;
+            // 
+            // btnAsync
+            // 
+            this.btnAsync.Location = new System.Drawing.Point(24, 170);
+            this.btnAsync.Margin = new System.Windows.Forms.Padding(2);
+            this.btnAsync.Name = "btnAsync";
+            this.btnAsync.Size = new System.Drawing.Size(159, 31);
+            this.btnAsync.TabIndex = 10;
+            this.btnAsync.Text = "Run ( ASync )";
+            this.btnAsync.UseVisualStyleBackColor = true;
+            // 
+            // btnExample1
+            // 
+            this.btnExample1.Location = new System.Drawing.Point(24, 101);
+            this.btnExample1.Margin = new System.Windows.Forms.Padding(2);
+            this.btnExample1.Name = "btnExample1";
+            this.btnExample1.Size = new System.Drawing.Size(159, 31);
+            this.btnExample1.TabIndex = 9;
+            this.btnExample1.Text = "Run ( Sync )";
+            this.btnExample1.UseVisualStyleBackColor = true;
+            // 
+            // rtbDisplay
+            // 
+            this.rtbDisplay.Location = new System.Drawing.Point(288, 73);
+            this.rtbDisplay.Margin = new System.Windows.Forms.Padding(2);
+            this.rtbDisplay.Name = "rtbDisplay";
+            this.rtbDisplay.Size = new System.Drawing.Size(297, 269);
+            this.rtbDisplay.TabIndex = 8;
+            this.rtbDisplay.Text = "";
+            this.rtbDisplay.TextChanged += new System.EventHandler(this.rtbDisplay_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnCallBack);
+            this.Controls.Add(this.btnReturnValue);
+            this.Controls.Add(this.btnStopWatch);
+            this.Controls.Add(this.btnMultiThread);
+            this.Controls.Add(this.btnAsync);
+            this.Controls.Add(this.btnExample1);
+            this.Controls.Add(this.rtbDisplay);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnCallBack;
+        private System.Windows.Forms.Button btnReturnValue;
+        private System.Windows.Forms.Button btnStopWatch;
+        private System.Windows.Forms.Button btnMultiThread;
+        private System.Windows.Forms.Button btnAsync;
+        private System.Windows.Forms.Button btnExample1;
+        private System.Windows.Forms.RichTextBox rtbDisplay;
+    }
+}
+
---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,371 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace CustomCPU_Code
+{
+    public partial class Form1 : Form
+    {
+
+        // NOTES:
+        //ThreadSafety: Locks up the data before fully purchasing a shopping cart to notify other people different threads/lanes where the other user is.,
+        //Syncronized means they're moving together in unison (coordinated)
+        public Form1()
+        {
+            InitializeComponent();
+            DisplayToRTB("Display Message"); //Testing I can display text in my RichTextBox
+
+            // Two new topics
+            // Returning a value async
+            // Callback 
+
+
+        } // Form1()
+
+
+
+        // ---------------- Returning Values - Start
+
+        // A callback is when a method is called after another event finishes
+        private async void btnCallBack_Click(object sender, EventArgs e)
+        {
+            //Task<int> task = Task.Run(async () =>
+            //{
+            //    DisplayToRTB("Task is running");
+            //    await Task.Delay(3000);
+            //    DisplayToRTB("Task is is finished");
+
+            //    return 5000;
+            //});
+
+            //// The argument passed in, represents the value that is returned from the instanced Task
+            //// you can get the result with the .Result property
+            //await task.ContinueWith( returnedValueFromTask =>
+            //{
+            //    DisplayToRTB("Continue is running");
+            //    int sum = returnedValueFromTask.Result + returnedValueFromTask.Result;
+            //    DisplayToRTB("Continue is finished");
+            //    DisplayToRTB(sum.ToString());
+            //});
+
+            // Super summed up callback
+            Task doubleSum = Task.Run(async () => // Created a task
+            {
+                DisplayToRTB("Start Run");
+                await Task.Delay(3000); // I delayed the task by 3 seconds
+                return 2.5; // I returned a value of 2.5
+            }).ContinueWith(async t => // I added a CALLBACK with .ContinueWith() directly to my original Task
+            {
+                DisplayToRTB("Start Sum");
+                await Task.Delay(3000);
+                DisplayToRTB((t.Result * t.Result).ToString());// I squared the result and returned it
+            });
+
+
+            //int number = await task;
+
+            //DisplayToRTB(number.ToString());
+        }
+
+        private async void btnReturnValue_Click(object sender, EventArgs e)
+        {
+            DisplayToRTB("Before we call our async method");
+            // use await to let your async method return a specific type
+            double sum = await AddNumbers(5, 5);
+
+            DisplayToRTB("After we call");
+
+            DisplayToRTB(sum.ToString());
+        }
+
+        // Make this async
+        // It will return a Task<double>
+        public async Task<double> AddNumbers(double number1, double number2)
+        {
+            Stopwatch sw = new Stopwatch();
+            int sum = 0;
+
+            sw.Start();
+            await Task.Run(() =>
+            {
+                for (int i = 0; i < 5000000; i++)
+                {
+                    Random rand = new Random();
+                    int randomNum1 = rand.Next(0, 2000);
+                    Random rand2 = new Random(randomNum1);
+                    int randomNum2 = rand.Next(0, randomNum1);
+                    Random rand3 = new Random(randomNum2);
+                    int randomNum3 = rand.Next(0, randomNum2);
+
+                    sum += randomNum1;
+
+                }
+                DisplayToRTB("The for loop just stopped running");
+            });
+            sw.Stop();
+            DisplayToRTB(sw.ElapsedMilliseconds.ToString());
+
+            return sum;
+        }
+
+
+
+        public async void CallBackExample()
+        {
+            Task<int> call = Task.Run(async () =>
+            {
+                await Task.Delay(3000);
+                return 10;
+            });
+
+            await call.ContinueWith(t =>
+            {
+                DisplayToRTB(t.Result + t.Result + "");
+            });
+        }
+
+
+        public async Task<double> PerformMath(double num1, double num2)
+        {
+
+            await Task.Delay(5000);
+
+            return num1 + num2;
+        }
+
+        public async void ExampleAsyncReturn()
+        {
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            double value = await PerformMath(1, 2);
+            sw.Stop();
+
+            DisplayToRTB($"Time Elapsed: {sw.Elapsed.TotalSeconds} : Result: {value}");
+        }
+
+        // ----------------- Returning Values - End
+
+        public void DisplayToRTB(string message)
+        {
+            rtbDisplay.Text += message + "\n"; // "/n" means New Line 
+        } // DisplayToRtb
+
+        public void LongProcess()
+        {
+            DisplayToRTB("Long Process Started");
+
+            // This is locking up the Main thread that the GUI is on
+            Thread.Sleep(6000); // 1 thousand milliseconds equals 1 second
+
+            DisplayToRTB("Long Process Ended");
+
+
+        } // LongProcess
+
+        // Create another method for ShortProcess()
+        public void ShortProcess()
+        {
+            DisplayToRTB("Short Process Started");
+            DisplayToRTB("Short Process Ended");
+        } // ShortProcess()
+
+        private void btnExample1_Click(object sender, EventArgs e)
+        {
+            ShortProcess();
+            LongProcess();
+
+        }
+
+        // Build our async methods
+        // async - method modifier - Tells the computer that a method runs in a special way
+        // await
+        // Tasks
+        public async void LongAsync(int number)
+        {
+            DisplayToRTB($"LongAsync started : Thread {number}");
+
+            // Replace Thread.Sleep with the async version
+
+            await Task.Delay(4000);
+
+            DisplayToRTB($"LongAsync Ended : Thread {number}");
+
+        } // LongAsync
+
+        private void btnAsync_Click(object sender, EventArgs e)
+        {
+            LongAsync(1);
+            ShortProcess();
+        } // btnAsync_Click
+
+        private void btnMultiThread_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                LongAsync(i);
+            }
+        } // btnMultiThread_Click
+
+        private void btnStopWatch_Click(object sender, EventArgs e)
+        {
+            LoopAsync();
+        } // btnStopWatch_Click
+
+        public void StopwatchExample()
+        {
+            Stopwatch sw = new Stopwatch();
+            // Start my stopwatch
+            sw.Start(); // Starts the stop watch
+
+            int multiplier = 10 * 10 * 10 * 10 * 10 * 10;
+            for (int i = 0; i < 100 * multiplier; i++)
+            {
+                Random rand = new Random();
+                int randomNum1 = rand.Next(0, 1000000);
+                Random rand2 = new Random(randomNum1);
+                int randomNum2 = rand.Next(0, randomNum1);
+                Random rand3 = new Random(randomNum2);
+                int randomNum3 = rand.Next(0, randomNum2);
+
+            }
+
+
+            // Stop my stopwatch
+            sw.Stop(); // Stops the stop watch
+
+            DisplayToRTB(sw.ElapsedMilliseconds.ToString());
+        } // Stopwatch Example
+
+        public async void LoopAsync()
+        {
+            Stopwatch sw = new Stopwatch();
+            // Start my stopwatch
+            sw.Start(); // Starts the stop watch
+
+            int multiplier = 10 * 10 * 10 * 10 * 10;
+
+            await Task.Run(() =>
+            {
+                for (int i = 0; i < 100000; i++)
+                {
+                    Random rand = new Random();
+                    int randomNum1 = rand.Next(0, 1000000);
+                    Random rand2 = new Random(randomNum1);
+                    int randomNum2 = rand.Next(0, randomNum1);
+                    Random rand3 = new Random(randomNum2);
+                    int randomNum3 = rand.Next(0, randomNum2);
+
+                }
+                DisplayToRTB("The for loop just stopped running");
+            });
+
+            // Stop my stopwatch
+            sw.Stop(); // Stops the stop watch
+
+            DisplayToRTB(sw.ElapsedMilliseconds.ToString());
+        } // LoopAsync
+
+        Action<int> loop = (s) =>
+        {
+            for (int i = 0; i < 100 * s; i++)
+            {
+                Random rand = new Random();
+                int randomNum1 = rand.Next(0, 1000000);
+                Random rand2 = new Random(randomNum1);
+                int randomNum2 = rand.Next(0, randomNum1);
+                Random rand3 = new Random(randomNum2);
+                int randomNum3 = rand.Next(0, randomNum2);
+
+            }
+        };
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            rtbDisplay.Text = "";
+        }
+
+        private void rtbDisplay_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+        // Stopwatch : What is it and why is it so important
+        // Stopwatch Object
+        // stopwatch.Start()
+        // stopwatch.Stop()
+        // stopwatch.Elapsed.Milliseconds
+
+        // -------------------
+
+
+        //async void LongTask()
+        //{
+        //    DisplayToRTB("LongTask Started");
+
+        //    await Task.Delay(4000);
+
+        //    DisplayToRTB("LongTask Ended");
+
+        //}
+
+        //void LongProcess()
+        //{
+        //    DisplayToRTB("LongProcess Started");
+
+        //    //some code that takes long execution time 
+        //    Thread.Sleep(4000); // hold execution for 4 seconds
+
+        //    DisplayToRTB("LongProcessEnded");
+        //} // LongProcess
+
+        //void ShortProcess()
+        //{
+        //    DisplayToRTB("ShortProcess Started");
+
+        //    //do something here
+
+        //    DisplayToRTB("ShortProcess Completed");
+        //} // ShortProcess()
+
+
+
+        //public void DisplayToRTB(string message)
+        //{
+        //    rtbDisplay.Text += message;
+        //    rtbDisplay.Text += "\n";
+        //} // DisplayToRTB
+
+        //private void btnExample1_Click(object sender, EventArgs e)
+        //{
+        //    Stopwatch sw = new Stopwatch();
+
+        //    sw.Start();
+
+        //    DisplayToRTB("Start of Example");
+        //    LongProcess();
+        //    ShortProcess();
+        //    DisplayToRTB("End of Example");
+
+        //    sw.Stop();
+
+        //    DisplayToRTB("-------------------" + sw.Elapsed.TotalSeconds.ToString());
+        //}
+
+        //private void btnAsync_Click(object sender, EventArgs e)
+        //{
+        //    LongTask();
+        //    ShortProcess();
+        //}
+    } // class
+
+} // namespace
